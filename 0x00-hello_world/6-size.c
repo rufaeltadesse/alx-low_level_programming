6710179dc273c68c42dc38c8a26b2e3796a259bb
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-16 07:13:09 -0800

    Push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,17 @@
 /* 
  * Size of variables
  */
-
-int main() {
+int main(void)
+{
 	char charType;
 	int intType;
 	long int longIntType;
 	long long int longLongIntType;
 	float floatType;
-	printf("Size of a char: %lu bytes\n", sizeof(charType));
-	printf("Size of an int: %lu bytes\n", sizeof(intType));
-	printf("Size of a long int: %lu bytes\n", sizeof(longIntType));
-	printf("Size of a long long int: %lu bytes\n", sizeof(longLongIntType));
-	printf("Size of a float: %lu bytes\n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 	return 0;
 }
