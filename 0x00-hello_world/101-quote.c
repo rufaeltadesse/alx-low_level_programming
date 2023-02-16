84d128230724880835265ddf7d1a00b66e9ad86d
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-16 07:57:11 -0800

    Push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	/*Function for printing fputs */
-	fputs("and that piece of art is useful\"", stdout);
-       	fputs("- Dora Korpar, 2015-10-19", stdout);
+	fputs("and that piece of art is useful\" ", stdout);
+	fputs("- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
