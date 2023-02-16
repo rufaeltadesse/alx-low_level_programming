f5015a97ed65083459361da4eb05b168d76f3cea
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-16 06:23:55 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	//string initialisation
+     
+    puts("Programming is like building a multilingual puzzle"); //writing the string to stdout
+    
+    return 0;
+}
