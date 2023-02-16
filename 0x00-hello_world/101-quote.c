012e67f0124abb2741a25ad53fdee8940c7ca08c
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-16 07:48:00 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	/*Function for printing fputs */
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
