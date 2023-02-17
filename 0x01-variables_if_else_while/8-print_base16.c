81f3b7d3c7b7bd3a22d8948839c3f2e9d6bc548f
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-17 12:00:30 -0800

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 103; i++)
+	{
+		if (i == 58)
+			i=97;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
