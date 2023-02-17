ce5e8701f64ab8ccaaaa19074d4a194f6f7422d9
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-17 10:50:11 -0800

    Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
         for (i = 97; i < 123; i++)
-        {
-                putchar(i);
-        }
+	{
+		putchar(i);
+	}
 	for (i = 65; i < 91; i++)
 	{
 		putchar(i);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
