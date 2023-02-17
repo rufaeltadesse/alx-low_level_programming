3a44420f8b6efa60df0b74a4cb60a80f398e0899
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-17 10:33:03 -0800

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* 
+ * Entry - main
+ * more headers goes there 
+ * betty style doc for function main goes there */
 int main(void)
 {
 	int i;
+	
 	for(i=97;i<123;i++)
+	{
 		putchar(i);
+	}
 	putchar('\n');
-	/* your code goes there */
 	return (0);
 }
