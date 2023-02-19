a935e012cf1c662a2093b0109b54054fe4d5ff51
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-19 04:42:39 -0800

    Print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,20 +2,21 @@
 #include<stdlib.h>
 /**
  * main - Program entry
- * Desc : writes a program which 
+ * Desc : writes a program which
  * displays list of numbers
  * Returns: 0
  */
 int main(void)
 {
 	int aa;
-	int bb=0;
+	int bb = 0;
+
 	while (bb < 10)
 	{
 		aa = 0;
 		while (aa < 10)
 		{
-			if (bb !=aa && bb < aa)
+			if (bb != aa && bb < aa)
 			{
 				putchar('0' + bb);
 				putchar('0' + aa);
