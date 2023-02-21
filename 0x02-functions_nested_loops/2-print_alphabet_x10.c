e5cd3bddcb6dd27b3a2b2d6e111a24a242f1e5a9
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-21 03:23:30 -0800

    Alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j=0;
+	int j = 0;
 	
-	while(j<10)
+	while (j < 10)
 	{
-		for(i = 97; i <= 122; i++)
+		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
 		j++;
-	}	
+	}
 }
