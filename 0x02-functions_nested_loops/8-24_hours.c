dcc299088cba8605b8f0f79d7d646b21ae50149b
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-21 05:58:30 -0800

    Jack Baeuer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * jack_bauer - prints hours
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0;i < 3; i++)
+	{
+		for(j = 0;j < 10; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+			if (i == 2 && j==3)
+                        {
+                                j = 10;
+                        }
+		}
+	}
+}
