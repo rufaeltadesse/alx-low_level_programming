ae1ee940c15748941a5170072806ea97dcbc24d6
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-21 10:59:47 -0800

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,17 +10,17 @@ void times_table(void)
 	int i;
 	int j;
 	int mult;
-	int lastDigit,firstDigit;
+	int lastDigit, firstDigit;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			mult = i * j;
-			if(mult>9)
+			if (mult > 9)
 			{
 			lastDigit = mult % 10;
-			firstDigit=mult;
+			firstDigit = mult;
 			while (firstDigit >= 10)
 			{
 				firstDigit = firstDigit / 10;
@@ -37,9 +37,9 @@ void times_table(void)
 			{
 			if (j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				}
 			_putchar(mult + '0');
 			}
