1aa2ac6b7baf916bc1509057442184b883bab84e
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-21 03:19:03 -0800

    Alphabet 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ * Description - alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j=0;
+	
+	while(j<10)
+	{
+		for(i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}	
+}
