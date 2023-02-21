529061a93a573bcc14744a87ad27bf2e73e87737
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-21 03:42:28 -0800

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * int _islower - is lower
- *
+ * islower - is lower
+ * c is a character
  * Return: 1
  */
 int _islower(int c)
 {
-	if(islower(c))
+	if (islower(c))
 	{
 		return (1);
 	}
