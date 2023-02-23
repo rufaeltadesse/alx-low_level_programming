6f145666c7a47758cd2823aafa01ad79c9c9c31f
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-23 00:00:03 -0800

    Traiangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - Entry
+ * @size: is a variable
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = size - 1; k > i; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
