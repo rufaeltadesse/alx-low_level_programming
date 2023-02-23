dd0f782a071715abef84530dc83fa18943cecd4a
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-23 01:26:53 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,6 +12,10 @@ int main(void)
 		if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			printf("Buzz");
+			else
 			printf("Buzz ");
 		else
 			printf("%d ", i);
