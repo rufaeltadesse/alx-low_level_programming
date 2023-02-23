77910d46c689e89da2bb7c845585eebd59af4027
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-22 23:01:48 -0800

    6 Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Entry
+ * @n: is a variable
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
