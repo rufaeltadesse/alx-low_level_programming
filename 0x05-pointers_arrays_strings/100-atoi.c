cb843045b6daff1937ca5c84b19e6fcef54b789b
rufaeltadesse <rufael.tadesse@gmail.com>
2023-02-27 06:42:41 -0800

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,17 @@ int _atoi(char *s)
 {
 	int i;
 	int j = strlen(s);
-	unsigned int at = 0;
+
 	int count = 0;
 
 	for (i = 0; i < j; i++)
 	{
 		if ((*(s + i) >= 48 && *(s + i) <= 57))
+		{
+			s = s + i;
+			break;
+		}
+		else if (*s == '-' && i == 0 && j > 0 && (*(s + 1) >= 48 && *(s + 1) <= 57))
 		{
 			s = s + i;
 			break;
@@ -26,9 +31,7 @@ int _atoi(char *s)
 		}
 	}
 	if (count > 0 && count % 2 != 0)
-	at =  -1 * atoi(s);
+	return (-1 * atoi(s));
 	else
-	at = atoi(s);
-
-	return (at);
+	return (atoi(s));
 }
