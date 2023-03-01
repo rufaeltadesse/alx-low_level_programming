ed9af03167501a44ea497430eb07fbe63ca9382a
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-01 08:02:08 -0800

    Cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * cap_string - entry
+ * @str: is a variable
+ * Return: 0
+ */
+char *cap_string(char *str)
+{
+	int size = strlen(str);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i < size - 1 && (str[i] == ';' || str[i] == '\n' || str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '{' || str[i] == '}' || str[i] == '?' || str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '\t'))
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+return (str);
+}
