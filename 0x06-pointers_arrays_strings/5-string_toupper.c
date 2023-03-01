cbba5325907dfe88fb2d604f28ed8d0aa60fe338
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-01 07:19:23 -0800

    To Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - entery
+ * @str: is a variable
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	int size = sizeof(str);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
