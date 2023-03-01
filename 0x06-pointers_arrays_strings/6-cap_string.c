e2a4fad14f0472c28db10afd1301e2ae377c3573
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-01 11:06:23 -0800

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * cap_dstring - entry
+ * cap_string - entry
  * @str: is a variable
  * Return: 0
  */
@@ -13,13 +13,30 @@ char *cap_string(char *str)
 
 	for (i = 0; i < size; i++)
 	{
-	if (i < size - 1 && (str[i] == ';' || str[i] == '\n' || str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '{' || str[i] == '}' || str[i] == '?' || str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '\t'))
+	if (i < size - 1)
+	{
+		if (str[i] == ';' || str[i] == '\n' || str[i] == ' ' || str[i] == ',')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
+		else if (str[i] == '.' || str[i] == '{' || str[i] == '}' || str[i] == '?')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		else if (str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '\t')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
 	}
 return (str);
 }
