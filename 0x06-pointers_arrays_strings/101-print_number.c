ab5199b26ff157512e440193aa32de47e76adcb5
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-01 23:37:22 -0800

    Print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_number - entry
+ * @n: is a number
+ * Return: 0
+ */
+void print_number(int n)
+{
+int div;
+int ab = n;
+if (n < 0)
+	n = n * -1;
+for (div = 1; div <= n; div *= 10)
+;
+if (ab < 0)
+	_putchar('-');
+do {
+div /= 10;
+if (div == 0)
+{
+	_putchar('0');
+	n=0;
+}
+else
+{
+_putchar((n / div) +'0');
+n %= div;
+}
+} while (n);
+}
