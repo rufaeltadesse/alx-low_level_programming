4f42e17f142ee0fe920ff6444bb9992893b4e414
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-06 02:29:14 -0800

    Memset

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strchr - entry
+ * @s: is a variable
+ * @c: is a character
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
