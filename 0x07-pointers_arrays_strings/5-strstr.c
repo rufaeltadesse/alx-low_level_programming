ddbf626d149f98c806228fe23ee585492291a6e9
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-06 03:24:59 -0800

    Sub string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - entry
+ * @haystack: is a variable
+ * @needle: is a variable
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
