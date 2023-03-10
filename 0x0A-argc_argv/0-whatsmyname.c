8d1796abb785102576428a128eb459923152984b
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-09 23:26:09 -0800

    FileName

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: is a variable
+ * @argv: is a variable
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+argc = argc;
+printf("%s\n",argv[0]);
+return (0);
+}
