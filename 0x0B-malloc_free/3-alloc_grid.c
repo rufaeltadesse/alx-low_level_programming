169c3a598e083aa98fdbba42c8262c0766509fa2
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-13 01:51:41 -0700

    Alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,13 +21,18 @@ int **alloc_grid(int width, int height)
 		array = malloc((height) * sizeof(int *));
 		if (array == NULL)
 		{
-			free(array);
 			return (NULL);
 		}
 		else
 		{
 		for (i = 0; i < height; i++)
+		{
 			array[i] = malloc((width) * sizeof(int));
+			if (array[i] == NULL)
+			{
+				return (NULL);
+			}
+		}
 		return (array);
 		}
 	}
