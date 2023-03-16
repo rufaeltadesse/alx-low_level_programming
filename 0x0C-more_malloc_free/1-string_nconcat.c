17ef86a4a257d03015b23050d63efea691118eb6
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-16 02:01:59 -0700

    String Nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - entry
+ * @s1: is a variable
+ * @s2: is a variable
+ * @n: is a variable
+ * Return: variable
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *constr;
+	unsigned int lens1 = strlen(s1);
+	unsigned int lens2 = strlen(s2);
+	unsigned int i, j = 0;
+
+	if (n < lens2)
+	{
+		constr = malloc(sizeof(char) * (lens1 + n + 1));
+	}
+	else
+	{
+		constr = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	}
+
+	if (!constr)
+		return (NULL);
+	for (i = 0; i < lens1; i++)
+	{
+		constr[i] = s[i];
+		j++;
+	}
+	for (i = 0; i < lens2; i++)
+		constr[j] = s2[i];
+	return (constr);
+}
+
