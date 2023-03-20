aa6029e85d482f8e2f797addf7d38bf014de5a5d
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-20 06:20:14 -0700

    Dogs Def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOGS
+#define DOGS
 /**
  * struct dog - my dog description
  * @name: is name of the dog
@@ -12,6 +14,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+#endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
