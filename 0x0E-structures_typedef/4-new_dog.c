1e54a8982b26fd9c49f93b92819f0fa554727da0
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-20 06:44:09 -0700

    New Dow

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <string.h>
 #include <stdlib.h>
 /**
  * new_dog - main entry
@@ -11,14 +12,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *a;
 
-	a = malloc(sizeof(dog_t *));
-	if (a == NULL)
+	a = malloc(sizeof(dog_t));
+	if (a == NULL || name == NULL || owner == NULL || age < 0)
+	{
 		return (NULL);
+	}
 	else
 	{
-	a->name = name;
+	a->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (a->name == NULL)
+	{
+		return (NULL);
+	}
+	else
+		strcpy(a->name, name);
+	a->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (a->owner == NULL)
+	{
+		return (NULL);
+	}
+	else
+		strcpy(a->owner, owner);
 	a->age = age;
-	a->owner = owner;
 	}
 	return (a);
 }
