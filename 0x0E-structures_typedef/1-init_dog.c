822b2ec597b1274665467c497c80e1b81131646d
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-20 00:43:03 -0700

    Init dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - main entry
+ * @d: is the struct
+ * @name: is name of the dog
+ * @age: age of the dog
+ * @owner: is the owner
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
