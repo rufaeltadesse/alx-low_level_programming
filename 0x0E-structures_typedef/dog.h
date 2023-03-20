2886fdcb72db60dc71e732d7192e115db46effca
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-20 00:22:35 -0700

    Dog Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - my dog description
+ * @name: is name of the dog
+ * @age: is the age of the dog
+ * @owner: is the owner of the dog
+ *
+ * Description: main entry
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
