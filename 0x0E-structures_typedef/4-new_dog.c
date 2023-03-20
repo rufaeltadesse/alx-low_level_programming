56eda18aa677bbbbcf95230d07bbd8dd91c7511f
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-20 06:30:48 -0700

    New Dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *a;
 
-	a = malloc(sizeof(dog_t));
+	a = malloc(sizeof(dog_t *));
 	if (a == NULL)
 		return (NULL);
+	else
+	{
 	a->name = name;
 	a->age = age;
 	a->owner = owner;
+	}
 	return (a);
 }
