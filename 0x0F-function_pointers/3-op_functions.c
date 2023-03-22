86fc7b6e357cf9256779963ed8d8687a1afea4f9
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-22 03:45:58 -0700

    Calc

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,4 @@
+#include "3-calc.h"
 /**
  * op_add - entery
  * @a: is a variable
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,17 +1,8 @@
 #ifndef STDDEF
 #define STDDEF
 #include <stddef.h>
-#endif
-#ifndef PRINTNAME
-#define PRINTNAME
 void print_name(char *name, void (*f)(char *));
-#endif
-#ifndef PRINTNAME1
-#define PRINTNAME1
 void array_iterator(int *array, size_t size, void (*action)(int));
-#endif
-#ifndef PRINTNAME2
-#define PRINTNAME2
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
 
