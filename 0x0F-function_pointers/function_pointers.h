3981fee7a4bd6c2dff84364b8925a0ad4a37060c
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-22 02:16:02 -0700

    Print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,9 +5,11 @@
 #ifndef PRINTNAME
 #define PRINTNAME
 void print_name(char *name, void (*f)(char *));
+#endif
 #ifndef PRINTNAME1
 #define PRINTNAME1
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
 #ifndef PRINTNAME2
 #define PRINTNAME2
 int int_index(int *array, int size, int (*cmp)(int));
