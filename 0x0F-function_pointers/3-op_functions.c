394d5a7f7839417b4abdad9527dc2aaea84b6243
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-22 02:13:28 -0700

    Function pointers.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - entery
+ * @a: is a variable
+ * @b: is a variable
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entery
+ * @a: is a variable
+ * @b: is a variable
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - entery
+ * @a: is a variable
+ * @b: is a variable
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - entery
+ * @a: is a variable
+ * @b: is a variable
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - entery
+ * @a: is a variable
+ * @b: is a variable
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,15 @@
+#ifndef STDDEF
+#define STDDEF
 #include <stddef.h>
+#endif
+#ifndef PRINTNAME
+#define PRINTNAME
 void print_name(char *name, void (*f)(char *));
+#ifndef PRINTNAME1
+#define PRINTNAME1
 void array_iterator(int *array, size_t size, void (*action)(int));
+#ifndef PRINTNAME2
+#define PRINTNAME2
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
 
