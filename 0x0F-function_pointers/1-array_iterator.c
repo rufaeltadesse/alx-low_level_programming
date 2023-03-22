2cb3ed878c043049d278a4a527642e4950ba7603
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-22 02:29:24 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (array == NULL || action == NULL || size <= 0)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(*(array + i));
