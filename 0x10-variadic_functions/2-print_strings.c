287e95da7e3cc0afcff1ce9c394b18d8914a33e0
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-23 03:59:18 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,19 +15,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *stringsList;
 
-	if (separator == NULL)
-		return;
 	va_start(sumList, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		stringsList = va_arg(sumList, char *);
 		if (stringsList != NULL)
 		{
-			if (i < n - 1)
-		       	printf("%s%s", stringsList, separator);
+			if (i < n - 1 && separator != NULL)
+				printf("%s%s", stringsList, separator);
 			else
-				printf("%s\n",stringsList);
+				printf("%s\n", stringsList);
 		}
 		else
 		{
