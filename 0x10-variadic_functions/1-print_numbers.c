0d27b7355502b387215bc3e5c8ce9c923f648697
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-23 03:51:21 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int sumTotal = 0;
 
-	if (separator == NULL)
-		return;
 	va_start(sumList, n);
 	for (i = 0; i < n; i++)
 	{
 		sumTotal = va_arg(sumList, int);
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%d%s", sumTotal, separator);
 		else
 			printf("%d", sumTotal);
