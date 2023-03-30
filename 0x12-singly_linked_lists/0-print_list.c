f5b30fd01a6197215a3247f22b0bf2d2819e9994
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-30 01:39:02 -0700

    Betty Adjustment

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@ size_t print_list(const list_t *h)
 {
 	const list_t *traverse = h;
 	int counter = 0;
+
 	while (traverse)
 	{
 	if (traverse->str == NULL)
 		printf("[0] (nil)");
 	else
-	printf("[%d] %s",traverse->len, traverse->str);
+	printf("[%d] %s", traverse->len, traverse->str);
 	printf("\n");
 	traverse = traverse->next;
 	counter++;
