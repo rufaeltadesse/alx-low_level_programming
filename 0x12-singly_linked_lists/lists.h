ba750cfdb55a4f2e6722878a6c108ce1fab3d4b2
rufaeltadesse <rufael.tadesse@gmail.com>
2023-03-30 02:29:54 -0700

    Betty djustment

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,21 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
 #include <stddef.h>
+/**
+ * struct listt - main entry
+ * @str: isa varaible
+ * @len: is a length
+ * @next: is a pointer to next
+ */
 typedef struct listt
 {
-        char *str;
-        int len;
-        struct listt *next;
+char *str;
+int len;
+struct listt *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+#endif
