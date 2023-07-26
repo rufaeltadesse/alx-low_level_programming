1c4f2eb4e6490b432e739b3eef1c169d4f7e1653
rufaeltadesse <122888054+rufaeltadesse@users.noreply.github.com>
2023-07-27 00:04:53 +0300

    Make files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Calculating permiter."""
+
+def island_perimeter(grid):
+    """Returning perimeter"""
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
+
+    return perim
+
+
+def num_water_neighbors(grid, i, j):
+    """Returning calculated perimeter"""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
